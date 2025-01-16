7535557684e31922b76e99bd7eb08936ec7f1eff
DoHoaiNam_63HTTT1 <namdo6069@gmail.com>
2025-01-16 18:20:26 +0700

    add saving

--- Multimedia_ImageProcessing/Form1.cs
+++ Multimedia_ImageProcessing/Form1.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
-//from chinh
+//from chinh goc
 namespace Multimedia_ImageProcessing
 {
     public partial class Form1 : Form
