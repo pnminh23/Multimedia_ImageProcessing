06081afb0215b8c6b81d3c2305e65e09dc7775f1
DoHoaiNam_63HTTT1 <namdo6069@gmail.com>
2025-01-17 20:09:32 +0700

    them chinhMau

--- Multimedia_ImageProcessing/Form1.cs
+++ Multimedia_ImageProcessing/Form1.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
-//from chinh goc
+//from chinh goc 
 namespace Multimedia_ImageProcessing
 {
     public partial class Form1 : Form
